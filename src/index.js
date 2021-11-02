d895ceb928d02fcef8b63ad4362631efcce090fb
zdurr22 <zdurr22@gmail.com>
2021-11-02 17:34:59 -0400

    commit tree db8802de9fa30e14a3584489fbe1d1e0651e8118

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +10,39 @@ const Home = () => {
   );
 };
 
+const About = () => {
+  return (
+    <div>
+      <h1>This is about my component!</h1>
+    </div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+      </form>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <Home />,
+    <Router>
+      <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      </div>
+    </Router>,
+  // <Home />,
   document.getElementById('root')
 );
